d626e5be6e603950dbad421f08a370b5e87c4ca8
Crowdbotics <team@crowdbotics.com>
2021-08-11 15:33:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dependabot2108111_d_22868Identifier",
+  url: "https://dependabot2108111_d_22868Identifier.botics.co",
+  api: "https://dependabot2108111_d_22868Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
